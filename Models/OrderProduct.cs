a411126b7f5399b86ffa65e31c72c455c53ddeaf
ctucker0113 <134985404+ctucker0113@users.noreply.github.com>
2024-02-23 10:40:24 -0600

    Added Models folder and models for all entities

---
+++ Models/OrderProduct.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Bangazon.Models
+{
+	public class OrderProduct
+	{
+		public OrderProduct()
+		{
+		}
+	}
+}
+
---
+++ Models/PaymentType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Bangazon.Models
+{
+	public class PaymentType
+	{
+		public PaymentType()
+		{
+		}
+	}
+}
+
